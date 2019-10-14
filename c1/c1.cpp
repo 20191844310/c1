10da5ef4f1896f2d99cbe5a04fe3ceaaafaf3072
20191844310 <2996478688@qq.com>
2019-10-14 12:48:54 +0800

    git config --global user.email "2996478688@qq.com"  git config --global user.name "20191844310"

---
+++ c1/c1.cpp
@@ -0,0 +1,15 @@
+// c1.cpp : Defines the entry point for the console application.
+//
+
+#include<stdio.h>
+
+
+int main()
+{	int a;
+a=1;
+while(a<=10){
+	printf("**********\n");
+	a++;
+}
+	return 0;
+}
---
+++ ns/ns.cpp
@@ -0,0 +1,14 @@
+// ns.cpp : Defines the entry point for the console application.
+//
+
+#include <stdio.h>
+
+int main()
+{	int a;
+	a=1;
+	while(a<=10){
+		printf("**********");
+		a++;
+	}
+	return 0;
+}
