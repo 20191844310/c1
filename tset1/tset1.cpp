99e9c48a027c3df375e02cce276d4ab774967521
20191844310 <2996478688@qq.com>
2019-10-14 15:35:02 +0800

    test1

---
+++ tset1/tset1.cpp
@@ -0,0 +1,15 @@
+// tset1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main(int argc, char* argv[])
+{	int a;
+	a=1;
+	while(a<=10){
+		printf("**********");
+		a++;
+	}
+	return 0;
+}
