16affc61f88704e6be7523ac1555850c5b40a2b9
20191844310 <2996478688@qq.com>
2019-10-14 15:07:25 +0800

    test001

---
+++ test001/StdAfx.h
@@ -0,0 +1,19 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__98E71C82_CC59_4822_9917_CEBD60028643__INCLUDED_)
+#define AFX_STDAFX_H__98E71C82_CC59_4822_9917_CEBD60028643__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+
+// TODO: reference additional headers your program requires here
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__98E71C82_CC59_4822_9917_CEBD60028643__INCLUDED_)
---
+++ test001/test001.cpp
@@ -0,0 +1,17 @@
+// test001.cpp : Defines the entry point for the console application.
+//
+
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{	
+	int a;
+	a=1;
+	while(a<=10)
+	{
+		printf("**********\n");
+		a++;
+	}
+	return 0;
+
+}
